95be892aa9eba212ed77663da22c1f40a3fbbcd7
Pavel Larin <www.larinpavel@mail.ru>
2023-10-30 23:40:57 +0500

    BeanNameAware, PostConstruct, PreDestroy

--- src/main/java/com/lar/Doctor.java
+++ src/main/java/com/lar/Doctor.java
@@ -1,11 +1,15 @@
 package com.lar;
 
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope(scopeName = "prototype")
-public class Doctor implements Staff {
+public class Doctor implements Staff, BeanNameAware {
 
     public String getQualification() {
         return qualification;
@@ -28,5 +32,20 @@ public class Doctor implements Staff {
         System.out.println("Doctor is assisting");
     }
 
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Set Bean name method is called");
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("Post Construct Method is called");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("Pre Destroy Method is called");
+    }
 
 }
--- src/main/java/com/lar/Main.java
+++ src/main/java/com/lar/Main.java
@@ -13,10 +13,10 @@ public class Main {
         Doctor doctor = context.getBean(Doctor.class);
         doctor.assist();
         doctor.setQualification("MBBS");
-        System.out.println(doctor);
-
-        Doctor doctor1 = context.getBean(Doctor.class);
-        System.out.println(doctor1);
+//        System.out.println(doctor);
+//
+//        Doctor doctor1 = context.getBean(Doctor.class);
+//        System.out.println(doctor1);
 
         // Both give this result because of the use of the SINGLETON Bean scope!
 //        Doctor is assisting
